d92059525642640349b74336b351f744f09e0661
gello696 <240654@edu.p.lodz.pl>
2022-06-06 21:19:17 +0200

    Start

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Final_Project_WWW'`, () => {
+  it(`should have as title 'Final_Project_Site'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Final_Project_WWW');
+    expect(app.title).toEqual('Final_Project_Site');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Final_Project_WWW app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Final_Project_Site app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    NavComponent,
+    AsideComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
